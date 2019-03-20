5365f8072b83778602999288922929ff7a2b7f1e
Jemila Al-Ghani <jalghani@jemilas-mbp.attlocal.net>
2019-03-20 11:23:02 -0700

    testing proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,5 +2,8 @@ const proxy = require("http-proxy-middleware");
 require("dotenv").config();
 
 module.exports = app => {
-  app.use("/api", proxy({ target: "http://localhost:4000" }));
+  app.use(
+    "/api",
+    proxy({ target: "http://localhost:" + process.env.PORT || 4000 })
+  );
 };
