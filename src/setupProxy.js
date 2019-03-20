aa8e8085b003ac38c57c5769250e7d4b4d0eacd5
Jemila Al-Ghani <jalghani@jemilas-mbp.attlocal.net>
2019-03-19 23:37:26 -0700

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,8 @@
-const proxy = require("http-proxy-middleware");
-console.log("here", process.env.PORT);
-module.exports = app => {
-  app.use(
-    "/api",
-    proxy({ target: `http://localhost:${process.env.PORT || 4000}` })
-  );
-};
+// const proxy = require("http-proxy-middleware");
+// console.log("here", process.env.PORT);
+// module.exports = app => {
+//   app.use(
+//     "/api",
+//     proxy({ target: `http://localhost:${process.env.PORT || 4000}` })
+//   );
+// };
